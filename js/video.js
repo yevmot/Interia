263d2bad398da92ec3607800583ecbb26b4974bf
Eugene Motrych <motrihzenya@gmail.com>
2024-04-04 14:37:18 +0300

    Added a player that plays videos from YouTube

---
+++ js/video.js
@@ -0,0 +1,24 @@
+window.addEventListener('DOMContentLoaded', function() {
+    const play = document.querySelector('.play');
+    const video = document.querySelector('.video');
+    const clear = document.querySelector('.clear');
+    const fade = document.querySelector('.fade');
+
+    const body = document.body;
+
+    play.addEventListener('click', function () {
+        play.classList.toggle('open')
+        video.classList.toggle('open')
+        clear.classList.toggle('open')
+
+        document.body.classList.toggle('no-scroll');
+    })
+
+    clear.addEventListener('click', function () {
+        play.classList.toggle('open')
+        video.classList.toggle('open')
+        clear.classList.toggle('open')
+
+        document.body.classList.toggle('no-scroll');
+    })
+});
